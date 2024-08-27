10a3c0868c35c4217244ebd886f91adc54aaaf23
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2024-08-27 16:57:41 +0900

    feat: #6 - 문서작성 webview ui 사전 구현

---
+++ app/(tabs)/document/documetWrite.tsx
@@ -0,0 +1,43 @@
+import PrevButton from '@/components/common/PrevButton';
+import MapWebView from "@/components/extraInfo/MapWebView";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useRef } from 'react';
+import { StyleSheet, View } from "react-native";
+import WebView, { WebViewMessageEvent } from 'react-native-webview';
+import { WebViewSource } from 'react-native-webview/lib/WebViewTypes';
+
+const DocumentWritePage = () => {
+  const webViewRef = useRef(null);
+
+  const onMessage = (event: WebViewMessageEvent) => {
+    const data = JSON.parse(event.nativeEvent.data);
+  };
+  const {url} = useLocalSearchParams();
+  return (
+    <View style={styles.container}>
+      <PrevButton isLogo={false} />
+      <WebView
+        style={styles.container}
+        source={{ uri: url as string }}
+        scalesPageToFit={false}
+        maximumZoomScale={0.8}
+        minimumZoomScale={0.8}
+      />
+    </View>
+  );
+};
+
+export default MapWebView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 6,
+    width: "100%",
+    height: 100,
+    marginTop: 24,
+  },
+  webView: {
+    flex: 1,
+    width: "100%",
+  },
+});
