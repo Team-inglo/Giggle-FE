e5378240c3f535f338d7df736a121dea24d64206
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-28 11:56:53 +0900

    feat: 여권 ocr 및 등록 api 연결

--- constants/Users.ts
+++ constants/Users.ts
@@ -1,19 +1,16 @@
 export interface UserInfoState {
   여권번호: string;
   이름: string;
-  체류자격: string;
-  체류기간: string;
-  발급일: string;
+  성별: string;
+  생년월일: string;
+  발급일만료일: string;
   국적: string;
 }
 
 export interface userRegistrationCardState {
   등록번호: string;
-  이름: string;
-  국적: string;
   체류자격: string;
   발급일자: string;
-  발급번호: string;
 }
 
 export interface employInfo {
