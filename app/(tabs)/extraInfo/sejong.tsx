58bc3a94898b3d000c88e7b3d2bcfbcc1b253a8f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-28 20:10:33 +0900

    feat: 세종학당 OCR API 연결

--- app/(tabs)/extraInfo/sejong.tsx
+++ app/(tabs)/extraInfo/sejong.tsx
@@ -5,19 +5,90 @@ import { SkipModal } from "@/components/signup/InvalidModal";
 import UploadPassPort from "@/components/signup/UploadPassport";
 import { ThemedView } from "@/components/ThemedView";
 import { SignupContext } from "@/store/signupContext";
-import { useRouter } from "expo-router";
-import { useContext, useState } from "react";
+import {useLocalSearchParams, useRouter} from "expo-router";
+import { useContext, useEffect, useState} from "react";
 import { StyleSheet, useWindowDimensions } from "react-native";
+import axios from "axios";
+import * as ImageManipulator from "expo-image-manipulator";
 
 const SignUpPage = () => {
+  const { access_token } = useLocalSearchParams();
+  const { sejong_institute_score,updateSignupData } = useContext(SignupContext)
   const { height } = useWindowDimensions();
-  const { sejong } = useContext(SignupContext);
   const [modalVisible, setModalVisible] = useState(false);
   const [imageUri, setImageUri] = useState<string | null>(null);
   const router = useRouter();
-  const handleButtonClick = () => {
-    imageUri !== null && router.push("/extraInfo/degree");
+
+  useEffect(() => {
+    if(imageUri !=null) sejongInstituteRegistration(imageUri);
+  }, [imageUri]);
+
+  const sejongInstituteRegistration = async (imageUri: any) => {
+    const manipResult = await ImageManipulator.manipulateAsync(
+        imageUri,
+        [{ resize: { width: 800, height: 1066 } }], // 크기를 800x1066으로 조정
+        { compress: 1, format: ImageManipulator.SaveFormat.PNG } // 압축하지 않고, JPEG 포맷으로 저장
+    );
+    const formData = new FormData();
+    const file = {
+      uri:manipResult.uri,
+      name: 'photo.png',
+      type: 'image/png',
+    } as unknown as Blob
+
+    formData.append('file', file);
+    try {
+      const response = await axios.post(
+          "https://api.giggle-inglo.com/api/v1/applicants/ocr/sejong-institute",
+          formData,
+          {
+            headers: {
+              "Authorization": `Bearer ${access_token}`,
+              "Content-Type": "multipart/form-data",
+            },
+          }
+      );
+      updateSignupData({ sejong_institute_score: response.data.data} ) ;
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
+  const updateSejongInstitute = async () => {
+    try {
+      const response = await axios(`https://api.giggle-inglo.com/api/v1/applicants/sejong-institute`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${access_token}`,
+          "Content-Type": "application/json",
+        },
+        data: {
+          "sejong_institute_score": sejong_institute_score
+        }
+      });
+      const success = await response.data.success;
+      if(success) {
+        router.push({
+          pathname:"/extraInfo/degree",
+          params: {
+            access_token: access_token
+          }
+        });
+      }
+    } catch (error) {
+      console.error("세종학당 등록 에러", error);
+    }
+  }
+  const handleButtonClick = () => {
+    const isAllInfoFilled = sejong_institute_score!=="";
+    if (isAllInfoFilled) {
+      updateSejongInstitute();
+    } else {
+      setModalVisible(false);
+    }
+  };
+  // const handleButtonClick = () => {
+  //   imageUri !== null && router.push("/extraInfo/degree");
+  // };
   return (
     <>
       <ThemedView style={[styles.background, { height }]}>
@@ -28,7 +99,7 @@ const SignUpPage = () => {
           title={"를\n등록해주세요."}
           description="맞춤형 아르바이트 광고 제공을 위해 필요해요."
         />
-        <InfoItem label="등급" value={String(sejong)} />
+        <InfoItem label="등급" value={String(sejong_institute_score)} />
         <UploadPassPort
           title="세종학당 증명서를 올려주세요."
           description="5MB 이하만 가능합니다. (png, jpeg 파일)"
